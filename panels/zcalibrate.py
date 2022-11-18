f3803c6d34ee7b67090ee5bb386122692bfb0302
Cyril <guilouz6@hotmail.com>
2022-11-18 23:28:46 +0100

    Update zcalibrate.py

--- panels/zcalibrate.py
+++ panels/zcalibrate.py
@@ -28,7 +28,7 @@ class ZCalibratePanel(ScreenPanel):
                                           _("Please remove leveling switch before move Z0."),
                                           "printer.gcode.script", script)
         self.labels['home'] = self._gtk.ButtonImage("home", _("Home"), "color1")
-        self.labels['home'].connect("clicked", self.home)
+        self.labels['home'].connect("clicked", self.go_to_home, "home")
         self.labels['z+'] = self._gtk.ButtonImage("z-farther", _("Raise Nozzle"), "color1")  
         self.labels['z+'].connect("clicked", self.change_babystepping, "+")
         self.labels['zoffset'] = self._gtk.ButtonImage("refresh", " Z-Offset: 0.00" + _("mm"),
@@ -84,6 +84,9 @@ class ZCalibratePanel(ScreenPanel):
             gcode = "SET_GCODE_OFFSET Z_ADJUST=-%s MOVE=1" % self.bs_delta
         self._screen._ws.klippy.gcode_script(gcode)
 
+    def go_to_home(self, widget, home):
+        gcode = "G28"
+        self._screen._ws.klippy.gcode_script(gcode)
 
     def change_bs_delta(self, widget, bs):
         if self.bs_delta == bs:
